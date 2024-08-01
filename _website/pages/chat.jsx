e7b68a1171cd0561d811753d9f54fb12d4441344
Leppy-oss <tyryanosaur@gmail.com>
2024-08-01 13:02:49 -0500

    remove suggestion and similar content on message start

--- _website/pages/chat.jsx
+++ _website/pages/chat.jsx
@@ -1,4 +1,4 @@
-import { Center, Stack, Image, ScrollArea, Container, Paper, Group } from '@mantine/core';
+import { Center, Stack, Image, ScrollArea, Container, Paper, Group, Box } from '@mantine/core';
 import HumanInput from '../components/search';
 import { postWithHandling } from '../lib/fetch-ex';
 import { useState } from 'react';
@@ -8,9 +8,13 @@ import classes from '../styles/chat.module.css';
 import { IconCircuitMotor, IconFlame, IconGauge, IconUsb } from '@tabler/icons-react';
 import { DEFAULT_THEME as theme } from '@mantine/core';
 
-const Suggestion = ({ children, mobile }) => (
-    <Paper className={classes.suggestion} radius='lg' p='md' withBorder w={mobile ? '50%' : '25%'}>
-        <Stack gap='xs'>{children}</Stack>
+const Suggestion = ({ Icon, children, mobile, colors, shouldTransition }) => (
+    <Paper className={`${classes.suggestion} ${!shouldTransition ? classes.noTransition : ''}`} radius='lg' p='md' withBorder w={mobile ? '50%' : '25%'}>
+        <Stack c='dimmed' gap='xs'>
+            <Box lightHidden><Icon stroke={1} color={colors[0]} /></Box>
+            <Box darkHidden><Icon stroke={1} color={colors[1]} /></Box>
+            {children}
+        </Stack>
     </Paper>
 );
 
@@ -30,29 +34,45 @@ export default function Chat() {
     return (
         <Stack w='100%' mt='md' h='100%'>
             <ScrollArea mah='100%' flex='1'>
-                <Center w='100%' pos='absolute' h='100%'>
+                <Center w='100%' pos='absolute' h='100%' style={{ visibility: chatHistory.length ? 'hidden' : 'visible' }}>
                     <Container p={0} w={mobile ? '90%' : '70%'}>
                         <Image src='/chat-banner.png' alt='' opacity={0.5} lightHidden />
                         <Image src='/chat-banner.png' alt='' opacity={1.0} darkHidden />
                         <Group mt='md' wrap='nowrap' align='flex-start'>
-                            <Suggestion mobile>
-                                <IconCircuitMotor stroke={1} color={theme.colors.blue[3]} />
-                                How many motors are allowed on a robot?
-                            </Suggestion>
-                            <Suggestion mobile>
-                                <IconGauge stroke={1} color='yellow' />
-                                Can I overclock a servo to 7.4V?
-                            </Suggestion>
-                            {!mobile && <>
-                                <Suggestion mobile>
-                                    <IconUsb stroke={1} color='pink' />
-                                    Is an external USB power bank allowed?
-                                </Suggestion>
-                                <Suggestion mobile>
-                                    <IconFlame stroke={1} color='orange' />
-                                    Is it illegal to set fire to an opponent?
-                                </Suggestion>
-                            </>}
+                            {[
+                                {
+                                    icon: IconCircuitMotor,
+                                    text: 'How many motors are allowed on a robot?',
+                                    colorLight: theme.colors.blue[6],
+                                    colorDark: theme.colors.blue[3]
+                                },
+                                {
+                                    icon: IconGauge,
+                                    visMob: true,
+                                    text: 'Can I overclock a servo to 7.4V?',
+                                    colorLight: theme.colors.yellow[5],
+                                    colorDark: theme.colors.yellow[3]
+                                },
+                                {
+                                    icon: IconUsb,
+                                    text: 'Is an external USB power bank allowed?',
+                                    colorLight: theme.colors.pink[5],
+                                    colorDark: theme.colors.pink[3]
+                                },
+                                {
+                                    icon: IconFlame,
+                                    visMob: true,
+                                    text: 'Is it illegal to set fire to a robot?',
+                                    colorLight: theme.colors.orange[5],
+                                    colorDark: theme.colors.orange[3]
+                                },
+                            ].map(suggestion => (!mobile || suggestion.visMob) && <Suggestion mobile
+                                key={suggestion.text}
+                                Icon={suggestion.icon}
+                                colors={[suggestion.colorDark, suggestion.colorLight]}
+                                shouldTransition={!chatHistory.length}>
+                                {suggestion.text}
+                            </Suggestion>)}
                         </Group>
                     </Container>
                 </Center>
@@ -64,6 +84,8 @@ export default function Chat() {
                         e.preventDefault();
                         setSubmitted(true);
                         setRecv(false);
+                        const initChatHistory = [];
+                        setChatHistory([...initChatHistory, query]);
                         const response = await postWithHandling('/api/inference/', { query, chat_history: chatHistory }, {
                             responseType: 'stream',
                             adapter: 'fetch'
@@ -79,7 +101,7 @@ export default function Chat() {
                                 setOutput(tempOutput);
                             }
                             console.log(tempOutput);
-                            setChatHistory([...chatHistory, query, tempOutput]);
+                            setChatHistory([...initChatHistory, query, tempOutput]);
                             setSubmitted(false);
                         }
                     }}
